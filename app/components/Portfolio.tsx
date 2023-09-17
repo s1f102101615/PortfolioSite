a1141cd941c57345285973a26185e7f5a834fc39
s1f102101615 <s1f102101615@iniad.org>
2023-09-17 12:07:37 +0900

    ポートフォリオ

--- app/components/Portfolio.tsx
+++ app/components/Portfolio.tsx
@@ -7,38 +7,39 @@ const Portfolio = () => {
   return (
     <div className={styles.base}>
   <div className={styles.container}>
-    <h1>ポートフォリオ</h1>
-    <h2>頑張って作った作品たちです</h2>
-    <div className={styles.box}>
+    <div className={styles.head}>
+        <h1 className={styles.title}>ポートフォリオ</h1>
+        <h2 className={styles.com}>頑張って作った作品たちです</h2>
+    </div>
+    <div className={styles.box1}>
       <div className={styles.imgholder}></div>
-      <h1 className="feature">オンラインオセロ</h1>
-      <p>詳細</p>
+      <h1 className={styles.feature}>オンラインオセロ</h1>
+      <p className={styles.detail}>詳細</p>
+    </div>
+    <div className={styles.box1}>
+      <div className={styles.imgholder}></div>
+      <h1 className={styles.feature}>グラディウス</h1>
+      <p className={styles.detail}>詳細</p>
+    </div>
+    <div className={styles.box1}>
+      <div className={styles.imgholder}></div>
+      <h1 className={styles.feature}>ProAppo</h1>
+      <p className={styles.detail}>詳細</p>
     </div>
     <div className={styles.box}>
       <div className={styles.imgholder}></div>
-      <h1 className="feature">グラディウス</h1>
-      <p>詳細</p>
+      <h1 className={styles.feature}>連絡あんてな</h1>
+      <p className={styles.detail}>詳細</p>
     </div>
     <div className={styles.box}>
       <div className={styles.imgholder}></div>
-      <h1 className="feature">ProAppo</h1>
-      <p>詳細</p>
-    </div>
-    <div className={styles.groupmargin}></div>
-    <div className={styles.box}>
-      <div className={styles.imgholder}></div>
-      <h1 className="feature">連絡あんてな</h1>
-      <p>詳細</p>
+      <h1 className={styles.feature}>マインスイーパー</h1>
+      <p className={styles.detail}>詳細</p>
     </div>
     <div className={styles.box}>
       <div className={styles.imgholder}></div>
-      <h1 className="feature">マインスイーパー</h1>
-      <p>詳細</p>
-    </div>
-    <div className={styles.box}>
-      <div className={styles.imgholder}></div>
-      <h1 className="feature">ポートフォリオ</h1>
-      <p>詳細</p>
+      <h1 className={styles.feature}>ポートフォリオ</h1>
+      <p className={styles.detail}>詳細</p>
     </div>
     <div className="group"></div>
   </div>
