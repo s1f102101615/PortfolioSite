3cc8bba0dadd520f9ede787d64ed1243da28d784
s1f102101615 <s1f102101615@iniad.org>
2023-09-19 16:27:18 +0900

    skill

--- app/components/Skills.tsx
+++ app/components/Skills.tsx
@@ -10,6 +10,20 @@ import html from '../../public/html.svg';
 import css from '../../public/css.svg';
 import ocaml from '../../public/ocaml.svg';
 import Image from 'next/image';
+import next from '../../public/next-js.svg';
+import react from '../../public/react-2.svg';
+import nodejs from '../../public/nodejs-icon.svg';
+import django from '../../public/django.svg';
+import postgre from '../../public/postgresql.svg';
+import mysql from '../../public/mysql-6.svg';
+import linux from '../../public/tux.svg';
+import docker from '../../public/docker-4.svg';
+import aws from '../../public/aws-2.svg';
+import github from '../../public/github-icon-1.svg';
+
+
+
+
 
 
 const Skills = () => {
@@ -17,8 +31,7 @@ const Skills = () => {
     <div className={styles.base}>
         <div className={styles.title}>Skill Set</div>
         <div className={styles.subtitle}>2023年9月現在</div>
-        <div className={styles.titles}>プログラミング言語</div>
-        <div className={styles.subtitles}>フロントエンド</div>
+        <div className={styles.subtitles}>プログラミング言語</div>
         <div className={styles.skill}>
             {/* 言語のアイコン */}
             <div className={styles.skill1}>
@@ -37,12 +50,6 @@ const Skills = () => {
                 <Image src={csharp} alt="C#" width={50} height={50} />
             </div>
             <div className={styles.skill1}>
-                <Image src={html} alt="Html" width={50} height={50} />
-            </div>
-            <div className={styles.skill1}>
-                <Image src={css} alt="Css" width={50} height={50} />
-            </div>
-            <div className={styles.skill1}>
                 <Image src={ocaml} alt="Ocaml" className={styles.now} width={50} height={50} />
             </div>
         </div>
@@ -50,53 +57,54 @@ const Skills = () => {
         <div className={styles.skill}>
             {/* 言語のアイコン */}
             <div className={styles.skill1}>
-                <div>HTML</div>
+            <Image src={next} alt="Next.js" width={50} height={50} />
             </div>
             <div className={styles.skill1}>
-                <div>HTML</div>
+            <Image src={react} alt="React" width={50} height={50} />
             </div>
             <div className={styles.skill1}>
-                <div>HTML</div>
+                <Image src={html} alt="Html" width={45} height={45} />
+            </div>
+            <div className={styles.skill1}>
+                <Image src={css} alt="Css" width={45} height={45} />
             </div>
         </div>
-        <div className={styles.subtitles}>フロントエンド</div>
+        <div className={styles.subtitles}>バックエンド</div>
         <div className={styles.skill}>
             {/* 言語のアイコン */}
             <div className={styles.skill1}>
-                <div>HTML</div>
+                <Image src={nodejs} alt="Node-js" width={50} height={50} />
             </div>
             <div className={styles.skill1}>
-                <div>HTML</div>
-            </div>
-            <div className={styles.skill1}>
-                <div>HTML</div>
+                <Image src={django} alt="Django" width={45} height={45} />
             </div>
         </div>
-        <div className={styles.subtitles}>フロントエンド</div>
+        <div className={styles.subtitles}>データーベース</div>
         <div className={styles.skill}>
             {/* 言語のアイコン */}
             <div className={styles.skill1}>
-                <div>HTML</div>
+                <Image src={postgre} alt="PostgreSQL" width={45} height={45} />
             </div>
             <div className={styles.skill1}>
-                <div>HTML</div>
-            </div>
-            <div className={styles.skill1}>
-                <div>HTML</div>
+                <Image src={mysql} alt="MySQL" width={45} height={45} />
             </div>
         </div>
-        <div className={styles.subtitles}>フロントエンド</div>
+        <div className={styles.subtitles}>その他</div>
         <div className={styles.skill}>
             {/* 言語のアイコン */}
             <div className={styles.skill1}>
-                <div>HTML</div>
+                <Image src={linux} alt="Linux" width={45} height={45} />
             </div>
             <div className={styles.skill1}>
-                <div>HTML</div>
+                <Image src={docker} alt="Docker" width={45} height={45} />
             </div>
             <div className={styles.skill1}>
-                <div>HTML</div>
+                <Image src={aws} alt="AWS" width={45} height={45} />
             </div>
+            <div className={styles.skill1}>
+                <Image src={github} alt="Github" width={45} height={45} />
+            </div>
+
         </div>
     </div>
   );
